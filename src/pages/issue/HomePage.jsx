859c49c549ce63cd9bb3c567259ef4c230a6d724
NavinZi <navinsbp@gmail.com>
2024-06-06 18:01:14 +0700

    update

--- src/pages/issue/HomePage.jsx
+++ src/pages/issue/HomePage.jsx
@@ -16,17 +16,16 @@ const HomePage = () => {
     <>
       <div className="max-w-full">
         <div className="flex h-[587px] justify-between px-[10%] items-center" style={{background: 'linear-gradient(135.86deg, #EAF5FF -2.07%, #E4F5FF 43.32%, #C9E6F9 68.38%, #BFDBF4 92.49%)'}}>
-          <div>
+          <div className="w-1/2">
             <div className="text-[88px] font-[700] text-[#1A3D93] leading-[94px] tracking-[0.1px] font-sarabun" style={borderStyle}>
             <p>Digital</p>
             <p>Credentials</p>
             </div>
-            <p className="font-roboto font-[600] text-[#757575] text-[20px] leading-[40px] tracking-[0.1px] mt-10">ตราสัญลักษณ์ใบรับรองคุณวุฒิดิจิทัลเป็นสินทรัพย์ดิจิทัลที่ใช้เพื่อถ่ายทอด</p>
-            <p className="font-roboto font-[600] text-[#757575] text-[20px] leading-[40px] tracking-[0.1px]">ความสำเร็จหรือคุณวุฒิทางการเรียนรู้สำเร็จหรือคุณวุฒิทางการเรียนรู้</p>
+            <p className="font-roboto font-[600] text-[#757575] text-[20px] leading-[40px] tracking-[0.1px] mt-10">ตราสัญลักษณ์ใบรับรองคุณวุฒิดิจิทัลเป็นสินทรัพย์ดิจิทัลที่ใช้เพื่อถ่ายทอดความสำเร็จหรือคุณวุฒิทางการเรียนรู้สำเร็จหรือคุณวุฒิทางการเรียนรู้</p>
             <button className="font-roboto font-[600] text-[16px] mt-6 p-2 w-[150px] bg-[#1A3D93] rounded-full text-white">ดาวน์โหลดแอป</button>
           </div>
           <div >
-          <img src={Group1} alt="home1"/>
+          <img src={Group1} alt="home1" className="w-[576px]"/>
           </div>
         </div>
         <div className="flex h-[347px] p-[10%] w-full justify-between items-center bg-white">
